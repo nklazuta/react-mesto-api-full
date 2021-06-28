9f634fb0c41064ea66769348d0e2a0f00202f4a5
N. Lazuta <nkgavshina@gmail.com>
2021-06-28 17:38:52 +0300

    fix: coookie option change

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -113,7 +113,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          sameSite: false,
         })
         .send({ data: user.toJSON() });
     })
