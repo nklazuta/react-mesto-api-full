7470b31ef8c991957a8f9ec27e45458bc7d13041
N. Lazuta <nkgavshina@gmail.com>
2021-08-10 19:51:15 +0300

    не возвращать пароль

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
 const isURL = require('validator/lib/isURL');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -30,18 +30,25 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email, password })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
       }
 
-      return user;
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
     });
 };
 
